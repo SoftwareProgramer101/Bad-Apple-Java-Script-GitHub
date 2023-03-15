1d720ddf39e5cd5f4d3b97579fbef0250cb7bcc2
SoftwareProgramer101 <97291683+SoftwareProgramer101@users.noreply.github.com>
2023-03-15 13:59:38 +0700

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -21,12 +21,38 @@ xui.Class('App', 'xui.Module',{
             var host=this, children=[], append=function(child){children.push(child.get(0));};
             
             append(
-                xui.create("xui.UI.Label")
-                .setHost(host,"xui_ui_label26")
-                .setLeft("25.904761904761905em")
+                xui.create("xui.UI.Image")
+                .setHost(host,"xui_ui_image7")
+                .setLeft("32em")
                 .setTop("19.80952380952381em")
-                .setWidth("12.342857142857143em")
-                .setCaption("Your server is unable! please use Asia VPN to acces this file")
+                .setSrc("{xui.ini.img_pic}")
+            );
+            
+            append(
+                xui.create("xui.UI.ProgressBar")
+                .setHost(host,"xui_ui_progressbar5")
+                .setLeft("22.095238095238095em")
+                .setTop("25.142857142857142em")
+            );
+            
+            append(
+                xui.create("xui.UI.Label")
+                .setHost(host,"xui_ui_label28")
+                .setLeft("26.666666666666668em")
+                .setTop("17.523809523809526em")
+                .setWidth("15.39047619047619em")
+                .setHeight("1.2190476190476192em")
+                .setCaption("Bad Apple!! made in Crossui.com")
+            );
+            
+            append(
+                xui.create("xui.UI.Label")
+                .setHost(host,"xui_ui_label29")
+                .setLeft("40.38095238095238em")
+                .setTop("26.666666666666668em")
+                .setWidth("6.247619047619048em")
+                .setHeight("1.980952380952381em")
+                .setCaption("Loading...")
             );
             
             return children;
